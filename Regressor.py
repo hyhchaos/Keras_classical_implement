2f06ee89014cdd9354c0a041d951705366f371c8
黄怿豪 <huangyihao22@sina.com>
2018-04-12 14:55:47 +0800

    add  comment

--- Regressor.py
+++ Regressor.py
@@ -1,39 +1,39 @@
 import numpy as np
 np.random.seed(1337)
-
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 
+#generate data
 X=np.linspace(-1,1,200)
 np.random.shuffle(X)
 Y=0.5*X+2+np.random.normal(0,0.05,(200,))
-
 plt.scatter(X,Y)
 plt.show()
-
 X_train,Y_train=X[:160],Y[:160]
 X_test,Y_test=X[160:],Y[160:]
 
+#build model
 model=Sequential()
-
 model.add(Dense(output_dim=1,input_dim=1))
 
+#compile model
 model.compile(loss="mse",optimizer="sgd")
 
+#train model
 print("Train Start")
 for step in range(301):
      cost=model.train_on_batch(X_train,Y_train)
      if step%100==0:
          print("Cost is : ",cost)
 
-
+#test model
 cost=model.evaluate(X_test,Y_test)
 print("Test cost : ",cost)
-
 W,b=model.layers[0].get_weights()
 print("W: ",W,"b: ",b)
 
+#visualize result
 Y_pred=model.predict(X_test)
 plt.figure()
 plt.scatter(X_test,Y_test)
